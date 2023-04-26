f0791367be590fbc732a5b61f9a11d1b7ee06200
Kuba <kubm2016@gmail.com>
2023-04-26 20:51:44 +0300

    second one

--- circle_text_animation/index.js
+++ circle_text_animation/index.js
@@ -1,7 +1,7 @@
-const text2 = document.querySelector(".text p");
+const text = document.querySelector(".text p");
 
-text2.innerHTML = text2.innerText.split("   ")
-text2.innerHTML = text2.innerText.split("").map((letter, i) =>
+text.innerHTML = text.innerText.split("   ")
+text.innerHTML = text.innerText.split("").map((letter, i) =>
 `<span style="transform:rotate(${i * 8}deg")>${letter}</span>`
 )
 .join(" ");
