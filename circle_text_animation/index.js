1731cde07575a386d8e0c25a1804d2b436bf6206
MatveyKuba <kubm2016@gmail.com>
2023-05-06 08:12:19 +0500

    TODO

--- circle_text_animation/index.js
+++
@@ -1,7 +0,0 @@
-const text = document.querySelector(".text p");
-
-text.innerHTML = text.innerText.split("   ")
-text.innerHTML = text.innerText.split("").map((letter, i) =>
-`<span style="transform:rotate(${i * 8}deg")>${letter}</span>`
-)
-.join(" ");
